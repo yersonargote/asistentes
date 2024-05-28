1cc7d83dbc4391d934a893ced7575b86991c7a10
yersonargote <yersonargotev@unicauca.edu.co>
2024-05-27 21:15:53 -0500

    chore: Update chat route to include a function for defining user's knowledge level

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,14 +1,29 @@
 import { google } from "@ai-sdk/google";
 import { streamText } from "ai";
+import { z } from "zod";
+
+async function definirNivelConocimiento({ nivel }: { nivel: string }) {
+  console.log(`El nivel de conocimiento del usuario es ${nivel}.`);
+  return `El nivel de conocimiento del usuario es ${nivel}.`;
+}
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await streamText({
-    model: google("models/gemini-1.5-flash-latest"),
-    system:
-      "Eres un asistente que hace preguntas al usuario e identifica que nivel de conocimiento tiene sobre programación orientada a objetos.",
+    model: google("models/gemini-1.5-pro-latest"),
+    system: `Eres un asistente que hace preguntas al usuario e identifica que nivel de conocimiento tiene sobre programación orientada a objetos.`,
     messages,
+    tools: {
+      definirNivelConocimiento: {
+        description:
+          "Define el nivel de conocimiento del usuario sobre programación orientada a objetos.",
+        parameters: z.object({
+          nivel: z.enum(["básico", "intermedio", "avanzado"]),
+        }),
+        execute: definirNivelConocimiento,
+      },
+    },
   });
 
   return result.toAIStreamResponse();
